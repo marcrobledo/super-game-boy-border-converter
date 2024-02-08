7b8a8020aa6ca310b2440126c2f8b6561eeac89f
Marc Robledo <mark@tinet.org>
2024-02-08 22:36:54 +0100

    first version

---
+++ app/sgb-converter.js
@@ -0,0 +1,385 @@
+import { Action, ColorZeroBehaviour, Dither, DitherPattern } from './tiledpalettequant/enums.js';
+import { PaletteSNES, ColorRGB15, Tile4BPP, Map } from './snes.js'
+
+
+/* app loading */
+$(document).ready((evt) => {
+	/* UI events */
+	$('#img-source').on('load', function(evt) {
+		var valid=(this.width===256 && this.height===224);
+		$('#btn-quantize').prop('disabled', !valid);
+		if(valid)
+			UI.notifications.empty()
+		else
+			UI.notifications.error('Invalid image dimensions (must be 256x224)');
+
+
+		var canvas=document.createElement('canvas');
+		var ctx=canvas.getContext('2d');
+		ctx.drawImage(this, 0, 0);
+		var pixels=[
+			ctx.getImageData(80, 80, 1, 1),
+			ctx.getImageData(176, 80, 1, 1),
+			ctx.getImageData(80, 134, 1, 1),
+			ctx.getImageData(176, 134, 1, 1)
+		]
+		var foundColor=true;
+		for(var i=1; i<4 && foundColor; i++){
+			if(
+				pixels[0].data[0]!==pixels[i].data[0] ||
+				pixels[0].data[1]!==pixels[i].data[1] ||
+				pixels[0].data[2]!==pixels[i].data[2] ||
+				pixels[0].data[3]!==pixels[i].data[3] ||
+				pixels[i].data[4]<255
+			){
+				foundColor=false;
+			}
+		}
+		
+		if(foundColor){
+			$('#input-color').val('#'+[pixels[0].data[0], pixels[0].data[1], pixels[0].data[2]].map(c => c.toString(16).padStart(2, '0')).join(''));
+		}
+	});
+	$('#img-source').on('error', function(evt) {
+		UI.notifications.error('Invalid image file');
+	});
+	$('#input-file').on('change', function(evt) {
+		if(!this.files || !this.files.length)
+			UI.notifications.error('Invalid file');
+
+		if (this.files.length > 0) {
+			document.getElementById('img-source').src=URL.createObjectURL(this.files[0]);
+		}
+	});
+	$('#btn-import').on('click', (evt) => {
+		$('#input-file').trigger('click');
+	});
+	$('#btn-quantize').on('click', function(){
+		$('main button').prop('disabled', true);
+
+
+		UI.notifications.empty();
+		const props={
+			numPalettes: 3,
+			colorZero: [255, 0, 255],
+			dithering: Dither.Off,
+			ditheringWeight: 0.5,
+			ditheringMethod: DitherPattern.Diagonal4
+		};
+		let numPalettes = parseInt(document.getElementById('num-palettes').value);
+		if(numPalettes===4){
+			UI.notifications.warning('Real hardware and LLE emulators don\'t support four palettes');
+			props.numPalettes=4;
+		}
+		quantizeSNESMap(document.getElementById('img-source'), props);
+	});
+
+	$('#btn-export-map').on('click', exportMap);
+	$('#btn-export-tiles').on('click', exportTiles);
+	$('#btn-export-palettes').on('click', exportPalettes);
+	$('#btn-export-sgb').on('click', exportSGB);
+
+	$('#select-dither').on('change', UI.toggleDitheringOptions);
+
+
+	UI.toggleDitheringOptions();
+});
+
+
+
+const UI={
+	notifications:{
+		empty:function(){
+			$('#alerts').empty();
+		},
+		add:function(message, className){
+			$('#alerts').append(
+				$('<div></<div>')
+					.addClass('alert'+(className? ' alert-'+className : ''))
+					.html(message)
+			)
+		},
+		error:function(message){
+			this.empty();
+			this.add(message, 'danger');
+		},
+		warning:function(message){
+			this.empty();
+			this.add(message, 'warning');
+		}
+	},
+	
+	toggleDitheringOptions:function(){
+		if(parseInt($('#select-dither').val())){
+			$('#container-dithering-options').show();
+		}else{
+			$('#container-dithering-options').hide();
+		}
+	}
+}
+
+
+
+
+
+
+/* tiledpalettequant web worker by rilden (https://rilden.github.io/tiledpalettequant/) that does the whole magic! */
+
+const DITHER_VALUES = [Dither.Off, Dither.Fast, Dither.Slow];
+const DITHER_PATTERN_VALUES = [
+	DitherPattern.Diagonal4,
+	DitherPattern.Horizontal4,
+	DitherPattern.Vertical4,
+	DitherPattern.Diagonal2,
+	DitherPattern.Horizontal2,
+	DitherPattern.Vertical2,
+];
+
+let worker = null;
+function quantizeSNESMap(sourceImage, props) {
+	let fractionOfPixels = parseFloat(document.getElementById('fraction-pixels').value);
+
+	let colorZeroValue = [255, 0, 255];
+	if(/^#[0-9a-f]{6}$/i.test(document.getElementById('input-color').value)){
+		const colorStr = document.getElementById('input-color').value;
+		colorZeroValue = [
+			parseInt(colorStr.slice(1, 3), 16),
+			parseInt(colorStr.slice(3, 5), 16),
+			parseInt(colorStr.slice(5, 7), 16),
+		];
+	}else{
+		//throw new Error('Invalid color value');
+	}
+
+
+	let dither = DITHER_VALUES[parseInt(document.getElementById('select-dither').value)];
+	let ditherWeight = parseFloat(document.getElementById('input-dithering-weight').value);
+	if (isNaN(ditherWeight)) {
+		ditherWeight = 0.5;
+		document.getElementById('input-dithering-weight').value = ditherWeight.toString();
+	}
+	let ditherPattern = DITHER_PATTERN_VALUES[parseInt(document.getElementById('select-dither-method'))];
+
+
+	if (worker)
+		worker.terminate();
+
+
+	worker = new Worker('./app/tiledpalettequant/worker.js');
+	worker.onmessage = function (event) {
+		const data = event.data;
+		if (data.action === Action.UpdateProgress) {
+			document.getElementById('progress-bar').value = data.progress;
+		}
+		else if (data.action === Action.DoneQuantization) {
+			secondStep();
+		}
+		else if (data.action === Action.UpdateQuantizedImage) {
+			const imageData = data.imageData;
+			const quantizedImageData = new window.ImageData(imageData.width, imageData.height);
+			for (let i = 0; i < imageData.data.length; i++) {
+				quantizedImageData.data[i] = imageData.data[i];
+			} 
+			const canvas=document.getElementById('canvas-map');
+			canvas.getContext('2d').putImageData(quantizedImageData, 0, 0);
+		}
+		else if (data.action === Action.UpdatePalettes) {
+			const palettes = data.palettes;
+			const PALETTE_TILE_SIZE = 16;
+			const canvas=document.getElementById('canvas-palettes');
+			//canvas.width = data.numColors * PALETTE_TILE_SIZE;
+			canvas.height = data.numPalettes * PALETTE_TILE_SIZE;
+			const palCtx = canvas.getContext('2d');
+			for (let j = 0; j < palettes.length; j += 1) {
+				for (let i = 0; i < palettes[j].length; i += 1) {
+					let colorString=`rgb(
+						${Math.round(palettes[j][i][0])},
+						${Math.round(palettes[j][i][1])},
+						${Math.round(palettes[j][i][2])})`;
+					palCtx.fillStyle = colorString;
+					palCtx.fillRect(i * PALETTE_TILE_SIZE, j * PALETTE_TILE_SIZE, PALETTE_TILE_SIZE, PALETTE_TILE_SIZE);
+					
+				}
+			}
+		}
+	};
+	
+	const canvas = document.createElement('canvas');
+	canvas.width = sourceImage.width;
+	canvas.height = sourceImage.height;
+	const ctx = canvas.getContext('2d');
+	ctx.drawImage(sourceImage, 0, 0);
+	const imageData=ctx.getImageData(0, 0, sourceImage.width, sourceImage.height);
+
+
+	$('#app').show();
+	$('#intro').hide();
+
+	worker.postMessage({
+		action: Action.StartQuantization,
+		imageData: imageData,
+		quantizationOptions: {
+			tileWidth: 8,
+			tileHeight: 8,
+			numPalettes: props.numPalettes,
+			colorsPerPalette: 16,
+			bitsPerChannel: 5,
+			fractionOfPixels: fractionOfPixels,
+			colorZeroBehaviour: ColorZeroBehaviour.Shared,
+			colorZeroValue: colorZeroValue,
+			dither: dither,
+			ditherWeight: ditherWeight,
+			ditherPattern: ditherPattern,
+		},
+	});
+};
+
+
+
+
+
+
+/* prepare SNES data from quantized images */
+var currentPalettes, currentTiles, currentMap;
+
+function findInTileset(tileset, tile){
+	for(var i=0; i<tileset.length; i++){
+		if(tileset[i]){
+			var tileMatch=tileset[i].equals(tile);
+			if(tileMatch)
+				return tileMatch;
+		}
+	}
+	return null;
+}
+
+function secondStep(){
+	var currentCanvas=document.getElementById('canvas-palettes');
+	var currentCtx=currentCanvas.getContext('2d');
+
+	/* parse palettes */
+	let numPalettes = parseInt(document.getElementById('num-palettes').value)
+	currentPalettes=new Array(numPalettes);
+	for(var y=0; y<numPalettes; y++){
+		currentPalettes[y]=new PaletteSNES();
+		for(var x=0; x<16; x++){
+			var imageData=currentCtx.getImageData(x*16, y*16, 1, 1).data;
+			currentPalettes[y].colors[x]=new ColorRGB15(imageData[0], imageData[1], imageData[2]);
+		}
+	}
+
+	/* parse tiles, build map */
+	currentCanvas=document.getElementById('canvas-map');
+	currentCtx=currentCanvas.getContext('2d');
+	currentTiles=new Array();
+	currentMap=new Map(32, 28);
+	var nDuplicates=0;
+	for(var y=0; y<28; y++){
+		for(var x=0; x<32; x++){
+			var imageData=currentCtx.getImageData(x*8, y*8, 8, 8).data;
+			var tile=Tile4BPP.fromImageData(imageData, currentPalettes);
+
+			var tileMatch=findInTileset(currentTiles, tile);
+			var attributes;
+			if(tileMatch){
+				currentMap.tiles[y*32 + x]=tileMatch.tile;
+				attributes={
+					paletteIndex:currentPalettes.indexOf(tile.palette),
+					flipX:tileMatch.flipValue & Tile4BPP.FLIP_X,
+					flipY:tileMatch.flipValue & Tile4BPP.FLIP_Y
+				};
+				nDuplicates++;
+			}else{
+				currentTiles.push(tile);
+
+				currentMap.tiles[y*32 + x]=tile;
+				attributes={
+					paletteIndex:currentPalettes.indexOf(tile.palette),
+					flipX:false,
+					flipY:false
+				};
+			}
+
+			currentMap.attributes[y*32 + x]=
+				((attributes.flipY? 1:0) << 7) |
+				((attributes.flipX? 1:0) << 6) |
+				((attributes.paletteIndex + 4) << 2) |
+				0x00;
+		}
+	}
+	currentCanvas=document.getElementById('canvas-tiles');
+	currentCtx=currentCanvas.getContext('2d');
+	//currentCanvas.width=128;
+	currentCanvas.height=Math.ceil(currentTiles.length/16) * 8;
+	for(var i=0; i<currentTiles.length; i++){
+		currentCtx.putImageData(currentTiles[i].toImageData(currentTiles[i].palette), (i%16) * 8, (Math.floor(i/16)) * 8);
+	}
+	if(currentTiles.length>256){
+		UI.notifications.error('More than 256 tiles');
+	}
+	
+	$('main button').prop('disabled', false);
+}
+
+
+
+function saveUint8Array(u8array, name){
+	var blob=new Blob([u8array.buffer], {type: 'application/octet-stream'});
+	saveAs(blob, name);
+}
+function buildDataMap(){
+	var u8array=new Uint8Array(currentMap.tiles.length * 2);
+	for(var i=0; i<currentMap.tiles.length; i++){
+		u8array[(i * 2) + 0]=currentTiles.indexOf(currentMap.tiles[i]);
+		u8array[(i * 2) + 1]=currentMap.attributes[i];
+	}
+	return u8array;
+}
+function exportMap(){
+	saveUint8Array(buildDataMap(), 'sgb_border_map.bin');
+}
+function buildDataTiles(){
+	var u8array=new Uint8Array(currentTiles.length * 32);
+	for(var i=0; i<currentTiles.length; i++){
+		for(var j=0; j<32; j++){
+			u8array[(i * 32) + j]=currentTiles[i].data[j];
+		}
+	}
+	return u8array;
+}
+function exportTiles(){
+	saveUint8Array(buildDataTiles(), 'sgb_border_tiles.bin');
+}
+function buildDataPalettes(){
+	var u8array=new Uint8Array(currentPalettes.length * 16*2);
+	for(var i=0; i<currentPalettes.length; i++){
+		for(var j=0; j<16; j++){
+			u8array[(i * 32) + (j* 2) + 0]=currentPalettes[i].colors[j].data & 0xff;
+			u8array[(i * 32) + (j* 2) + 1]=currentPalettes[i].colors[j].data >> 8;
+		}
+	}
+	return u8array;
+}
+function exportPalettes(){
+	saveUint8Array(buildDataPalettes(), 'sgb_border_palettes.bin');
+}
+function exportSGB(){
+	var u8array=new Uint8Array((256 * 32) + (32 * 28 * 2) + (16 * 2 * 4));
+
+	var dataTiles=buildDataTiles();
+	for(var i=0; i<dataTiles.length; i++){
+		u8array[0x0000 + i] = dataTiles[i];
+	}
+
+	var dataMap=buildDataMap();
+	for(var i=0; i<dataMap.length; i++){
+		u8array[0x2000 + i] = dataMap[i];
+	}
+
+	var dataPalettes=buildDataPalettes();
+	for(var i=0; i<dataPalettes.length; i++){
+		u8array[0x2700 + i] = dataPalettes[i];
+	}
+
+	saveUint8Array(u8array, 'my_border.sgb');
+}
---
+++ app/snes.js
@@ -0,0 +1,217 @@
+function PaletteSNES(){
+	this.colors=new Array(16);
+	for(var i=0; i<this.colors.length; i++)
+		this.colors[i]=new ColorRGB15(0, 0, 0);
+}
+
+PaletteSNES.prototype.getColorIndex=function(color){	
+	for(var i=0; i<this.colors.length; i++){
+		if(this.colors[i].equals(color))
+			return i;
+	}
+	return -1;
+}
+PaletteSNES.prototype.hasColor=function(color){
+	return this.getColorIndex(color)!==-1;
+}
+PaletteSNES.prototype.hasColors=function(colors){
+	for(var i=0; i<colors.length; i++){
+		if(!this.hasColor(colors[i]))
+			return false;
+	}
+	return true;
+}
+PaletteSNES.prototype.export=function(){
+	return this.colors.map((color) => {
+		return color.data;
+	});
+}
+
+
+function ColorRGB15(r8, g8, b8){
+	this.r8=r8;
+	this.g8=g8;
+	this.b8=b8;
+	this.r=ColorRGB15.to5bit(r8);
+	this.g=ColorRGB15.to5bit(g8);
+	this.b=ColorRGB15.to5bit(b8);
+
+	this.data=(this.b << 10) + (this.g << 5) + this.r;
+}
+ColorRGB15.prototype.equals=function(color2){
+	return this.data===color2.data;
+}
+
+
+ColorRGB15.RESCALE_24TO15BIT=8.22580645161291;
+ColorRGB15.BIT5_MASK=0x1f;
+
+ColorRGB15.to5bit=function(color){return Math.round(color/ColorRGB15.RESCALE_24TO15BIT) & ColorRGB15.BIT5_MASK}
+
+
+
+
+
+
+
+
+
+function Tile4BPP(){
+	this.data=[
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+	];
+
+	this.palette=new PaletteSNES();
+}
+Tile4BPP.prototype.toJSON=function(){
+	return this.data.concat([currentProject.palettes.indexOf(this.palette)]);
+}
+Tile4BPP.prototype.getPixel=function(row, col){
+	return(
+		((this.data[Math.floor(row*2) + 16 + 1] & Tile4BPP.COL_MASK[col]) << 3) +
+		((this.data[Math.floor(row*2) + 16] & Tile4BPP.COL_MASK[col]) << 2) +
+		((this.data[Math.floor(row*2) + 1] & Tile4BPP.COL_MASK[col]) << 1) +
+		((this.data[Math.floor(row*2)] & Tile4BPP.COL_MASK[col]))
+	) >> 7-col;
+}
+Tile4BPP.prototype.setPixel=function(row, col, colorIndex){
+	this.data[(row*2)]=(this.data[(row*2)] & Tile4BPP.COL_MASK_NEG[col]) | ((colorIndex & 0x01) << (7-col));
+	this.data[(row*2) + 1]=(this.data[(row*2) + 1] & Tile4BPP.COL_MASK_NEG[col]) | (((colorIndex >>> 1) & 0x01) << (7-col));
+
+	this.data[(row*2) + 16]=(this.data[(row*2) + 16] & Tile4BPP.COL_MASK_NEG[col]) | (((colorIndex >>> 2) & 0x01) << (7-col));
+	this.data[(row*2) + 17]=(this.data[(row*2) + 16 + 1] & Tile4BPP.COL_MASK_NEG[col]) | (((colorIndex >>> 3) & 0x01) << (7-col));
+}
+Tile4BPP.prototype.toImageData=function(palette){
+	var imageData=new ImageData(8, 8);
+	var k=0;
+	for(var i=0; i<8; i++){
+		for(var j=0; j<8; j++){
+			imageData.data[k++]=palette.colors[this.getPixel(i, j)].r8;
+			imageData.data[k++]=palette.colors[this.getPixel(i, j)].g8;
+			imageData.data[k++]=palette.colors[this.getPixel(i, j)].b8;
+			imageData.data[k++]=255;
+		}
+	}
+	return imageData;
+}
+Tile4BPP.prototype.buildFlippedData=function(){
+	this._flippedTiles=new Array(4);
+	this._flippedTiles[Tile4BPP.FLIP_NONE]=this.data;
+
+	this._flippedTiles[Tile4BPP.FLIP_X]=new Tile4BPP();
+	this._flippedTiles[Tile4BPP.FLIP_Y]=new Tile4BPP();
+	this._flippedTiles[Tile4BPP.FLIP_XY]=new Tile4BPP();
+
+	for(var y=0; y<8; y++){
+		for(var x=0; x<8; x++){
+			this._flippedTiles[Tile4BPP.FLIP_X].setPixel(y, x, this.getPixel(y, 7-x));
+			this._flippedTiles[Tile4BPP.FLIP_Y].setPixel(y, x, this.getPixel(7-y, x));
+			this._flippedTiles[Tile4BPP.FLIP_XY].setPixel(y, x, this.getPixel(7-y, 7-x));
+		}
+	}
+
+	this._flippedTiles[Tile4BPP.FLIP_X]=this._flippedTiles[Tile4BPP.FLIP_X].data;
+	this._flippedTiles[Tile4BPP.FLIP_Y]=this._flippedTiles[Tile4BPP.FLIP_Y].data;
+	this._flippedTiles[Tile4BPP.FLIP_XY]=this._flippedTiles[Tile4BPP.FLIP_XY].data;
+}
+Tile4BPP.prototype.equals=function(tile){
+	if(!this._flippedTiles)
+		this.buildFlippedData();
+
+	for(var i=0; i<this._flippedTiles.length; i++){
+		var found=true;
+		for(var j=0; j<32 && found; j++){
+			if(this._flippedTiles[i][j]!==tile.data[j])
+				found=false;
+		}
+		if(found){
+			return{
+				tile:this,
+				palette:tile.palette,
+				flipValue:i
+			}
+		}
+	}
+	return false;
+}
+Tile4BPP.FLIP_NONE=0b00000000;
+Tile4BPP.FLIP_X=0b00000001;
+Tile4BPP.FLIP_Y=0b00000010;
+Tile4BPP.FLIP_XY=Tile4BPP.FLIP_X | Tile4BPP.FLIP_Y;
+Tile4BPP.COL_MASK=[0x80, 0x40, 0x20, 0x10, 0x08, 0x04, 0x02, 0x01];
+Tile4BPP.COL_MASK_NEG=[0x7f, 0xbf, 0xdf, 0xef, 0xf7, 0xfb, 0xfd, 0xfe];
+
+
+Tile4BPP.fromImageData=function(imageData, possiblePalettes){
+	var allColors=new Array(64);
+	for(var i=0; i<64; i++){
+		allColors[i]=new ColorRGB15(imageData[i*4 + 0], imageData[i*4 + 1], imageData[i*4 + 2]);
+	}
+
+	var uniqueColors=allColors.reduce(function(acc, current){
+		let foundColor=false;
+		for(let i=0; i<acc.length; i++){
+			if(acc[i].equals(current))
+				return acc;
+		}
+		
+		if(!acc.includes(current))
+			acc.push(current);
+		return acc;
+	}, []);
+
+	var foundPalette=null;
+	for(var i=0; i<possiblePalettes.length && !foundPalette; i++){
+		if(possiblePalettes[i].hasColors(uniqueColors)){
+			foundPalette=possiblePalettes[i];
+		}
+	}
+	
+	if(!foundPalette)
+		throw new Error('no valid palettes for this tile');
+
+	var tile=new Tile4BPP();
+	for(var i=0; i<64; i++){
+		tile.setPixel(Math.floor(i/8), i%8, foundPalette.getColorIndex(allColors[i]));
+	}
+	tile.palette=foundPalette;
+	return tile;
+}
+
+
+
+
+
+function Map(w, h){
+	this.width=w;
+	this.height=h;
+	this.tiles=new Array(w * h);
+	this.attributes=new Array(w * h);
+}
+Map.prototype.getTile=function(x, y){
+	return this.tiles[y * this.width + x];
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export {PaletteSNES, ColorRGB15, Tile4BPP, Map };
